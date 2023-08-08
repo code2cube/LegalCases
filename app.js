f0fa591b636198ee4b228af5f7e4c56f29e676ca
Samuel Sharivker <114585707+samuels0052@users.noreply.github.com>
2023-08-07 21:39:14 -0400

    added base function

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+const PORT = 3030;
+
+app.use(express.json())
+
+app.listen(
+    PORT,
+    () => console.log(`alive on https://localhost:${PORT}`)
+)
+
+const keysFile = 'keys.json';
+const data = fs.readFileSync(keysFile, 'utf8');
+const keys = JSON.parse(data);
+const apiKeys = keys.apiKeys;
+
+app.get('/', (req, res) => {
+    res.status(404).send({
+        error: "Not Authenticated!"
+    })
+});
+
+app.get('/:key/:msg?', (req, res) => {
+    const rawKey = req.params['key'];
+    const rawMsg = req.params['msg'];
+    if(apiKeys.includes(rawKey)) {
+        res.status(200).send({
+            status: "online",
+            message: rawMsg
+        })
+    } else {
+        res.status(404).send({
+            error: "Invalid API Key!"
+        })
+    }
+});
